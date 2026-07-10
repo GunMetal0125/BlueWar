b0650a6d4736b08eed85c9064fd938422ad616da
GunMetal0125 <andy.arroyo@aol.com>
2026-07-10 01:17:54 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -14,3 +14,19 @@ void loop() {
   startBluetoothScan();
   delay(2000);
 }
+#include <Arduino.h>
+#include "bt.h"
+#include "display.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  initDisplay();
+  initBluetoothScanner();
+}
+
+void loop() {
+  startBluetoothScan();
+  delay(2000);
+}
